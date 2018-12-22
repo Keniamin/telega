b81e8dcf4ef17c44362249aec43ca54c5e5791c0
Keniamin <keniamin@users.noreply.github.com>
2018-12-22 23:49:13 +0000

    telega: improve install-requirements script

--- bin/install-requirements.sh
+++ bin/install-requirements.sh
@@ -1,2 +1,8 @@
-easy_install fresco pyyaml pymysql
-gem install sass
+#!/bin/bash
+mkdir -p /run/lock
+
+flock -x /run/lock/easy_install.lock\
+    easy_install fresco pymysql pyyaml
+
+flock -x /run/lock/gem.lock\
+    gem install sass
