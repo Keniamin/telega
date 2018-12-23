884707030c0f2669016a518c3fe4ee9d754134e9
Keniamin <keniamin@users.noreply.github.com>
2018-12-23 00:01:46 +0000

    telega: fix fresco version

--- bin/install-requirements.sh
+++ bin/install-requirements.sh
@@ -2,7 +2,7 @@
 mkdir -p /run/lock
 
 flock -x /run/lock/easy_install.lock\
-    easy_install fresco pymysql pyyaml
+    easy_install fresco==0.8.3 pymysql pyyaml
 
 flock -x /run/lock/gem.lock\
     gem install sass
